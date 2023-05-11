20005a1c427b1c35192a479cbd942d64a7ea5c67
unknown <stam296@gmail.com>
2023-05-11 16:11:25 -0500

    Added comments to script.js on code functionality. Updated readme file with a screenshot of the deployed application

--- README.md
+++ README.md
@@ -1,6 +1,35 @@
 # Weather-Dashboard-Application
 A dashboard to obtain weather information on the city that user searches for
 
+## Description
+This application takes user input of a city name (currently only works for cities in the USA) to display that city's current weather and 5-day weather forecast using data from the OpenWeather API.
+
+A search history is generated with each search and the user can click on any previously searched city to display the current weather and 5-day weather forecast of it. 
+
+## Technology Used
+* BootStrap
+* OpenWeather API
+* Visual Studio Code
+
+## Overview of Site
+![Screenshot of the deployed weather dashboard application](./assets/images/Weather%20Dashboard%20Screenshot.gif)
+
+## Deployed Application
+https://saratam8.github.io/Weather-Dashboard-Application/
+
+## Summary
+* Utilized BootStrap CSS framework
+* Utilized OpenWeather API for weather data
+
 ## Resources Used
 
 Used this link to get date from unix time: https://stackoverflow.com/questions/847185/convert-a-unix-timestamp-to-time-in-javascript
+
+OpenWeather API Documentation used to get icons: https://openweathermap.org/weather-conditions
+
+Current weather API call: https://openweathermap.org/current
+
+Weather forecast API call: https://openweathermap.org/forecast5
+
+## Author Links
+https://github.com/saratam8
--- assets/script.js
+++ assets/script.js
@@ -9,6 +9,7 @@ var cardTitle = document.querySelectorAll('.card-title');
 var currentIcon = document.getElementById('card-title-main');
 var apiKey = 'cef7710a3e1c8dc71a4ec2409ad5949f';
 
+// This function will take the user's input and pass the city name into geoCodeApi function and saveSearch function
 function getCity(event){
     event.preventDefault();
 
@@ -18,8 +19,10 @@ function getCity(event){
     saveSearch(city);
 }
 
+// eventlistener for the submit button. Will call getCity function when Submit button is clicked
 button.addEventListener('click', getCity);
 
+// This function will take user's input and save as a button
 function saveSearch(cityName){
     var saved = document.createElement('button');
     saved.textContent = cityName;
@@ -30,15 +33,20 @@ function saveSearch(cityName){
     citiesEl.appendChild(saved);
     savedbutton = document.querySelectorAll('.btn-secondary');
 
+    // For each saved search, there is an event listener for each of those buttons and when clicked, will get openweatherapp api data for the corresponding city
     for(var i = 0; i < savedbutton.length; i++){
-            savedbutton[i].addEventListener('click', function getHistory(){
-            var savedName = this.id;
-            geoCodeApi(savedName);
+            savedbutton[i].addEventListener('click', function getHistory(event){
+                event.preventDefault();
+                var savedName = this.id;
+                geoCodeApi(savedName);
         });
     }
 
 }
 
+// This function calls the geocoding portion of openweatherapp. User can type in a city name and the api will return the longitude and latitude of the city
+// The longitude and latitude are then passed into weatherApi function and forecastApi function
+// Currently set to only work for cities in the USA
 function geoCodeApi(city){
     var geoUrl = 'https://api.openweathermap.org/geo/1.0/direct?q=' + city + ',US&limit=5&appid=' + apiKey;
 
@@ -56,6 +64,7 @@ function geoCodeApi(city){
 
 }
 
+// This function uses coordinates from geoCodeApi function to get current weather data from openweatherapp api
 function weatherApi(coordinates){
     var weatherUrl = 'https://api.openweathermap.org/data/2.5/weather?lat=' + coordinates[0] + '&lon=' + coordinates[1] + '&units=imperial' +'&appid=' + apiKey;
 
@@ -67,7 +76,7 @@ function weatherApi(coordinates){
             var temp = data.main.temp;
             var windSpeed = data.wind.speed;
             var humidity = data.main.humidity;
-
+            // openweatherapi gives date/time in unix, the following lines are to convert unix to standard date
             var description = data.weather[0].main;
             var unix = data.dt;
             var currentDate = new Date(unix * 1000);
@@ -76,12 +85,14 @@ function weatherApi(coordinates){
             var year = currentDate.getFullYear();
 
             currentIcon.src = getIcon(description);
-
+            // Display data from openweatherapp api to webpage
             cardHeaderEl.textContent = `${data.name}  ${month}/${date}/${year}`;
             currentWeatherEl.innerHTML = `Temp: ${temp} °F <br /> Wind: ${windSpeed} mph <br /> Humidity: ${humidity} %`;
         });
 }
 
+// This function uses coordinates from geoCodeApi function to get forecasted data from openweatherapp api
+// Only forecast data available under a free subscription is the 5-day/3 hour forecast
 function forecastApi(coordinates){
     var forecastUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + coordinates[0] + '&lon=' + coordinates[1] + '&units=imperial' +'&appid=' + apiKey; 
 
@@ -95,27 +106,29 @@ function forecastApi(coordinates){
             var j = 0;
 
             for(var i = 7; i < dataLength; i += Math.floor(dataLength/5)){
-                    var unix = data.list[i].dt;
-                    var currentDate = new Date(unix * 1000);
-                    var month = currentDate.getMonth();
-                    var date = currentDate.getDate();
-                    var year = currentDate.getFullYear();
+                // openweatherapi gives date/time in unix, the following lines are to convert unix to standard date
+                var unix = data.list[i].dt;
+                var currentDate = new Date(unix * 1000);
+                var month = currentDate.getMonth();
+                var date = currentDate.getDate();
+                var year = currentDate.getFullYear();
+                // Following lines is assigning the weather data to variables
+                var temp = data.list[i].main.temp;
+                var windSpeed = data.list[i].wind.speed;
+                var humidity = data.list[i].main.humidity;
+                var description = data.list[i].weather[0].main;
+                // Display data from openweatherapp api to webpage
+                forecastDate[j].innerHTML = `${cityName}  ${month}/${date}/${year}`;
+                forecastEl[j].innerHTML = `Temp: ${temp} °F <br /> Wind: ${windSpeed} mph <br /> Humidity: ${humidity} %`;
+                cardTitle[j].src = getIcon(description);
 
-                    var temp = data.list[i].main.temp;
-                    var windSpeed = data.list[i].wind.speed;
-                    var humidity = data.list[i].main.humidity;
-                    var description = data.list[i].weather[0].main;
-
-                    forecastDate[j].innerHTML = `${cityName}  ${month}/${date}/${year}`;
-                    forecastEl[j].innerHTML = `Temp: ${temp} °F <br /> Wind: ${windSpeed} mph <br /> Humidity: ${humidity} %`;
-                    cardTitle[j].src = getIcon(description);
-
-                    j++;
+                j++;
 
             }
         });
 }
 
+//This function will look at the weather description given by openweatherapp api and choose an icon accordingly
 function getIcon(description){
     if(description == "Rain" || description == "Drizzle"){
         return "./assets/images/rain.png";
