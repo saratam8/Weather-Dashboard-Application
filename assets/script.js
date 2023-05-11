c99959797210d3269a5a502ffb8e214ae0afd93c
unknown <stam296@gmail.com>
2023-05-10 23:37:07 -0500

    Added icon images to assets folder. Added id attributes to elements in index.html. In javascript, implemented forecastApi function to retrieve and display 5 day forecast weather. getIcon function created so that the corresponding weather icon will be displayed for current and forecast weather.

--- assets/script.js
+++ assets/script.js
@@ -7,13 +7,31 @@ var input = document.querySelector('#input');
 var citiesEl = document.getElementById('cities');
 var cardHeaderEl = document.getElementById('header-today');
 var currentWeatherEl = document.getElementById('weather-today');
+var forecast1 = document.getElementById('forecast1');
+var forecast2 = document.getElementById('forecast2');
+var forecast3 = document.getElementById('forecast3');
+var forecast4 = document.getElementById('forecast4');
+var forecast5 = document.getElementById('forecast5');
+var date1 = document.getElementById('date1');
+var date2 = document.getElementById('date2');
+var date3 = document.getElementById('date3');
+var date4 = document.getElementById('date4');
+var date5 = document.getElementById('date5');
+var icon1 = document.getElementById('icon1');
+var icon2 = document.getElementById('icon2');
+var icon3 = document.getElementById('icon3');
+var icon4 = document.getElementById('icon4');
+var icon5 = document.getElementById('icon5');
+var currentIcon = document.getElementById('card-title-main');
+var forecastArray = [forecast1, forecast2, forecast3, forecast4, forecast5];
+var forecastDate = [date1, date2, date3, date4, date5];
+var iconArray = [icon1, icon2, icon3, icon4, icon5];
 var apiKey = 'cef7710a3e1c8dc71a4ec2409ad5949f';
 
 var getCity = function(event){
     event.preventDefault();
 
     var city = input.value;
-    // console.log(city);
     geoCodeApi(city);
     input.value = " ";
     saveSearch(city);
@@ -22,8 +40,6 @@ var getCity = function(event){
 button.addEventListener('click', getCity);
 
 function saveSearch(cityName){
-    console.log(citiesEl);
-    console.log(cityName);
     var saved = document.createElement('button');
     saved.textContent = cityName;
     saved.setAttribute("class", "btn btn-secondary btn-sm col-8 m-1");
@@ -34,18 +50,13 @@ function saveSearch(cityName){
 }
 
 function geoCodeApi(city){
-    var geoUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + city + ',US&limit=5&appid=' + apiKey;
-
-    // console.log('here');
+    var geoUrl = 'https://api.openweathermap.org/geo/1.0/direct?q=' + city + ',US&limit=5&appid=' + apiKey;
 
     fetch(geoUrl)
         .then(function (response){
             return response.json();
         })
         .then(function(data){
-            console.log(data);
-            console.log(data[0].lat);
-            console.log(data[0].lon);
             var coord = [];
             coord[0] = data[0].lat;
             coord[1] = data[0].lon;
@@ -63,8 +74,6 @@ function weatherApi(coordinates){
             return response.json();
         })
         .then(function(data){
-            console.log(data);
-            console.log(data.name);
 
             var temp = data.main.temp;
             var windSpeed = data.wind.speed;
@@ -72,21 +81,12 @@ function weatherApi(coordinates){
 
             var description = data.weather[0].main;
             var unix = data.dt;
-            // var unixToMilliseconds = unix * 1000;
             var currentDate = new Date(unix * 1000);
             var month = currentDate.getMonth();
             var date = currentDate.getDate();
             var year = currentDate.getFullYear();
 
-            console.log(currentDate);
-            console.log(month);
-            console.log(date);
-            console.log(year);
-
-            console.log(temp);
-            console.log(windSpeed);
-            console.log(humidity);
-            console.log(description);
+            currentIcon.src = getIcon(description);
 
             cardHeaderEl.textContent = `${data.name}  ${month}/${date}/${year}`;
             currentWeatherEl.innerHTML = `Temp: ${temp} °F <br /> Wind: ${windSpeed} mph <br /> Humidity: ${humidity} %`;
@@ -94,26 +94,53 @@ function weatherApi(coordinates){
 }
 
 function forecastApi(coordinates){
-    var forecastUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + coordinates[0] + '&lon=' + coordinates[1] + '&units=imperial' +'&appid=' + apiKey;
+    var forecastUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + coordinates[0] + '&lon=' + coordinates[1] + '&units=imperial' +'&appid=' + apiKey; 
 
     fetch(forecastUrl)
         .then(function (response){
             return response.json();
         })
         .then(function(data){
-            console.log(data);
-            // console.log(data.name);
-            // cardHeaderEl.textContent = data.name;
+            var cityName = data.city.name;
+            var dataLength = data.list.length;
+            var j = 0;
 
-            // var temp = data.main.temp;
-            // var windSpeed = data.wind.speed;
-            // var humidity = data.main.humidity;
-            // var description = data.weather[0].main;
+            for(var i = 7; i < dataLength; i += Math.floor(dataLength/5)){
+                    var unix = data.list[i].dt;
+                    var currentDate = new Date(unix * 1000);
+                    var month = currentDate.getMonth();
+                    var date = currentDate.getDate();
+                    var year = currentDate.getFullYear();
 
-            // console.log(temp);
-            // console.log(windSpeed);
-            // console.log(humidity);
-            // console.log(description);
-            // currentWeatherEl.innerHTML = `Temp: ${temp} °F <br /> Wind: ${windSpeed} mph <br /> Humidity: ${humidity} %`;
+                    var temp = data.list[i].main.temp;
+                    var windSpeed = data.list[i].wind.speed;
+                    var humidity = data.list[i].main.humidity;
+                    var description = data.list[i].weather[0].main;
+
+                    forecastDate[j].innerHTML = `${cityName}  ${month}/${date}/${year}`;
+                    forecastArray[j].innerHTML = `Temp: ${temp} °F <br /> Wind: ${windSpeed} mph <br /> Humidity: ${humidity} %`;
+                    iconArray[j].src = getIcon(description);
+
+                    j++;
+
+            }
         });
+}
+
+function getIcon(description){
+    if(description == "Rain" || description == "Drizzle"){
+        return "./assets/images/rain.png";
+    }
+    else if (description == "Clouds"){
+        return "./assets/images/clouds.png";
+    }
+    else if (description == "Clear"){
+        return "./assets/images/clear.png";
+    }
+    else if (description == "Snow"){
+        return "./assets/images/snow.png";
+    }
+    else if (description == "Thunderstorm"){
+        return "./assets/images/thunderstorm.png";
+    }
 }
